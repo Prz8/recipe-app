d457b1c35bb63bec7c3cf424b64a203323fad0a9
Prajit Panday <105464788+Prz8@users.noreply.github.com>
2022-05-13 12:23:47 -0500

    Configured files for deployment

---
+++ precache-manifest.02d79c4d4942679526b38937503fcc58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c59b076e0a42e74b2d5222e990b7b37",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "a8778df464b9d4eafec8",
+    "url": "/recipe-app/static/css/main.445105ee.chunk.css"
+  },
+  {
+    "revision": "5629887c69f69530656a",
+    "url": "/recipe-app/static/js/2.e8de8194.chunk.js"
+  },
+  {
+    "revision": "c87b86bb6afffe182bf9b4def1ac4811",
+    "url": "/recipe-app/static/js/2.e8de8194.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8778df464b9d4eafec8",
+    "url": "/recipe-app/static/js/main.58af846c.chunk.js"
+  },
+  {
+    "revision": "bc1330cd351eec214388",
+    "url": "/recipe-app/static/js/runtime-main.a99229d7.js"
+  },
+  {
+    "revision": "34aac984a796babb6b6398bc7c2476d5",
+    "url": "/recipe-app/static/media/background.34aac984.jpeg"
+  }
+]);
